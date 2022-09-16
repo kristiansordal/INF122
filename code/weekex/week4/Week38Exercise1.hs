eba8f392abbd83a66533139e4a823a03f6c22d9a
Kristian Sørdal <krisor99@gmail.com>
2022-09-16 12:27:47 +0200

    :sparkles: fininshed week38

--- code/weekex/week4/Week38Exercise1.hs
+++ code/weekex/week4/Week38Exercise1.hs
@@ -3,5 +3,7 @@ module Week38Exercise1 where
 mySplitAt :: Integer -> [a] -> ([a], [a])
 mySplitAt n [] = ([], [])
 mySplitAt n (x : xs)
-  | n >= 1 = ([], [])
-  | n < 1 = ([], x : xs)
+  | n <= 0 = ([], x : xs)
+  | otherwise = (x : firstHalf, secondHalf)
+  where
+    (firstHalf, secondHalf) = mySplitAt (n - 1) xs
