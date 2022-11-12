1acbfefdd480cea227c2b612ded92f39f46a2d9a
Kristian Sørdal <krisor99@gmail.com>
2022-11-12 16:47:47 +0100

    :sparkles:

--- code/exam/2017V.hs
+++ code/exam/2017V.hs
@@ -8,14 +8,38 @@ inList x = foldl (\acc y -> (x == y) || acc) False
 
 data Expr = V Int | M Expr Expr | D Expr Expr
 
+div' :: Int -> Int -> Maybe Int
+div' _ 0 = Nothing
+div' x y = Just (x `div` y)
+
 eval :: Expr -> Maybe Int
 eval (V x) = Just x
-eval (M r l) = do
-  -- eval r  eval l
-  -- let x =<< eval r
-  -- y <- eval l
-  return (Just x * y)
-eval (D r l) = do
-  x <- eval r
-  y <- eval l
-  x / y
+eval (M r l) =
+  case eval r of
+    Nothing -> Nothing
+    Just x -> case eval l of
+      Nothing -> Nothing
+      Just y ->
+        if x >= 0 && y >= 0
+          then Just (x * y)
+          else Nothing
+eval (D r l) =
+  case eval r of
+    Nothing -> Nothing
+    Just x -> case eval l of
+      Nothing -> Nothing
+      Just y -> div' x y
+
+toDoList :: [IO a] -> IO [a]
+toDoList [] = return []
+toDoList (x : xs) = do
+  action <- x
+  list' <- toDoList xs
+  return (action : list')
+
+mapActions :: (a -> IO b) -> [a] -> IO [b]
+mapActions f [] = return []
+mapActions f (x : xs) = do
+  y <- f x
+  ys <- mapActions f xs
+  return (y : ys)
